b881e3b104c8cfab2d66c561ef093a7d1a7f445f
Iveren Shaguy <iverenshaguy@gmail.com>
2018-04-23 17:08:32 +0100

    :wrench: set up server side

---
+++ server/src/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+
+// Set up the express app
+const app = express();
+
+const logger = morgan;
+
+// Log requests to the console.
+app.use(logger('dev'));
+
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+const port = process.env.PORT || 8000;
+
+app.listen(port);
+
+export default app;
